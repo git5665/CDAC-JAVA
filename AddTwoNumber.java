5d17a2d7f69cfdcba418c5a086174e4e54c2aded
vipul kumar <122309708+git5665@users.noreply.github.com>
2025-03-05 22:45:19 +0530

    Add files via upload

---
+++ AddTwoNumber.java
@@ -0,0 +1,14 @@
+class AddTwoNumber {
+
+    public static void add(){
+        int a = 2;
+        int b = 8;
+        int result = (a+b)*5;
+        System.out.println("Sum of "+a +" and " +b +" is " +result);
+        // return result;
+    }
+    public static void main(String arg[]) {
+
+            add();
+    }
+}
